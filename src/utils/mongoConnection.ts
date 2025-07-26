8216a306b65468c634094758aa18628bf98c2092
mahbub mollah <mollahmahbub81@gmail.com>
2025-07-26 12:38:16 +0600

    .env file

--- src/utils/mongoConnection.ts
+++ src/utils/mongoConnection.ts
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const uri =
-  "mongodb+srv://mollahmahbub81:EoT9O6fjn2zB0DYd@cluster0.euy6krx.mongodb.net/ghstudy-db";
 
 let isConnected = false;
 
@@ -9,7 +7,7 @@ export async function connectToMongoDB() {
   if (isConnected) return; // Prevent multiple connections in dev
 
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.DB as string);
     isConnected = true;
     console.log("Connected to MongoDB");
   } catch (err) {
